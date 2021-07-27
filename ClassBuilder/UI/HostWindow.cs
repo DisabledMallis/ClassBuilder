2a766f73b04fafa2cef5c5b2262de0e04a79c62d
DisabledMallis <mallisiscool@gmail.com>
2021-07-27 00:50:52 -0400

    Start UI

--- ClassBuilder/UI/HostWindow.cs
+++ ClassBuilder/UI/HostWindow.cs
@@ -1,41 +1,65 @@
 using System;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Forms;
 
-namespace ClassBuilder.UI {
+namespace ClassBuilder.UI
+{
 	public class HostWindow : Form {
 
 		public static HostWindow instance = new HostWindow();
 
-		private HostWindow() {
+		private HostWindow() : base() {
 			this.Text = "ClassBuilder";
+			this.MainMenuStrip = CreateMenuBar();
+			this.MainMenuStrip.Dock = DockStyle.Top;
+			this.Controls.Add(this.MainMenuStrip);
 
-			this.IsMdiContainer = true;
-					ToolStripMenuItem newProject = new ToolStripMenuItem();
-					newProject.Name = "newProject";
-					newProject.Text = "New";
-					ToolStripMenuItem openProject = new ToolStripMenuItem();
-					openProject.Name = "openProject";
-					openProject.Text = "Open";
-				ToolStripMenuItem fileDropdown = new ToolStripMenuItem();
-				fileDropdown.DropDownItems.AddRange(new ToolStripItem[] {
-					newProject,
-					openProject
-				});
-				fileDropdown.Name = "fileDropdown";
-				fileDropdown.Text = "File";
+			Panel contentPanel = new Panel();
+			contentPanel.Anchor = AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			int menuHeight = this.MainMenuStrip.Height;
+			contentPanel.Location = new Point(0, menuHeight);
+			contentPanel.Size = new Size(contentPanel.Width, this.Height-menuHeight);
+			contentPanel.Controls.Add(CreateClassList());
+
+			this.Controls.Add(contentPanel);
+		}
+
+		private MenuStrip CreateMenuBar() {
 			MenuStrip menu = new MenuStrip();
-			menu.Items.AddRange(new ToolStripItem[] {
-				fileDropdown
+			ToolStripMenuItem file = CreateFileTab();
+			menu.Items.AddRange(new ToolStripMenuItem[] {
+				file
 			});
-			menu.Name = "menu";
-			menu.Text = "Menu";
-			this.Controls.Add(menu);
-			this.MainMenuStrip = menu;
-			
-			ClassManager cm = new ClassManager();
-			cm.MdiParent = this;
-			cm.Show();
+			return menu;
+		}
+
+		private ToolStripMenuItem CreateFileTab() {
+
+			ToolStripMenuItem newProj = new ToolStripMenuItem();
+			newProj.Text = "New";
+
+			ToolStripMenuItem openProj = new ToolStripMenuItem();
+			openProj.Text = "Open";
+
+			ToolStripMenuItem file = new ToolStripMenuItem();
+			file.DropDownItems.AddRange(new ToolStripMenuItem[] {
+				newProj,
+				openProj
+			});
+
+			file.Text = "File";
+
+			return file;
+		}
+
+		private TreeView CreateClassList() {
+			TreeView classTree = new TreeView();
+
+			classTree.Dock = DockStyle.Left;
+			classTree.Nodes.Add(new TreeNode("Test"));
+
+			return classTree;
 		}
 	}
 }
